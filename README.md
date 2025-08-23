0e8e696a6f37e85dc63bf1b3f86b5523d4259b30
Robert Derveloy <rderveloy@gmail.com>
2025-08-23 13:53:11 -0400

    Adding setup.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # py-homonyms
 A comprehensive library for checking homonyms, homophones, and homographs in English.
+
+For non-commercial use only CC BY-NC-SA 4.0.
--- py_homonyms/homonyms_library.py
+++ py_homonyms/homonyms_library.py
@@ -6,3 +6,11 @@ A comprehensive library for checking homonyms, homophones, and homographs in Eng
 
 class HomonymsLibrary:
     pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
